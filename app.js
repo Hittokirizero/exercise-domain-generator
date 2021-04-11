f13597e539fd94f4a471a4bf4fc1a7d5fee22079
Hittokirizero <v_hbk@hotmail.com>
2021-04-11 17:21:40 +0000

    Final

--- app.js
+++ app.js
@@ -3,9 +3,20 @@ let pronoun = ['the','our'];
 let adj = ['great', 'big' ];
 let noun = ['jogger','racoon'];
 
-let domain =  pronoun[(Math.floor(Math.random() * pronoun.length))] +
-               adj[(Math.floor(Math.random() * adj.length))] +
-               noun[(Math.floor(Math.random() * noun.length))] + ".com"
-console.log(domain)
+let domain = " Revisar la consola :D "// pronoun[(Math.floor(Math.random() * pronoun.length))] +
+             // adj[(Math.floor(Math.random() * adj.length))] +
+             // noun[(Math.floor(Math.random() * noun.length))] + ".com"
+
+//console.log(pronoun.forEach(random_num(pronoun.length)))
+
+//let domain = pronoun.forEach(random_num(pronoun.length)) + adj.forEach(random_num(adj.length)) + noun.forEach(random_num(noun.length)) + ".com"
+
+//function random_num(i) {
+//    random_array = Math.floor(Math.random() * i)
+//    return random_array
+//}
+pronoun.forEach(item => adj.forEach(item2 => noun.forEach(item3 =>  console.log(item + item2 + item3 + ".com"))))
+
+
 document.querySelector("#mensaje").innerHTML = domain;
 }
